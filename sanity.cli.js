1037927dba52b85f3b3a69c99423b3e8dd225bb6
tk7g <atulgaa360@gmail.com>
2023-05-12 09:31:08 +0800

    feat: bootstrap sanity studio

---
+++ sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '6sy0p41x',
+    dataset: 'production'
+  }
+})
---
+++ sanity.config.js
@@ -0,0 +1,23 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+//import {googleMapsInput} from '@sanity/google-maps-input'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'blog-sanity',
+
+  projectId: '6sy0p41x',
+  dataset: 'production',
+
+  plugins: [
+    deskTool(),
+    visionTool(),
+    //googleMapsInput(),
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
